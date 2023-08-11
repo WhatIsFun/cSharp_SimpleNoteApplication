05288e515bfa743822ae3e23f5707da4980f2577
WhatIsFun <mohammed110051@gmail.com>
2023-08-11 22:38:48 +0400

    Menu

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace cSharp_SimpleNoteApplication
             Console.WriteLine("      Welcome To    ");
             Console.WriteLine(" +-+-+-+-+-+-+-+-+-+\r\n |W|h|a|t|I|s|F|u|n|\r\n +-+-+-+-+-+-+-+-+-+");
             Console.WriteLine("        Notes      \n");
+            Menu menu = new Menu();
+            menu.DisplayMenu();
 
         }
     }
