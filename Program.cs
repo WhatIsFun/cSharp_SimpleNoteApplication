67061a7de158cd5f3576c52f4e4b92cec670be16
WhatIsFun <mohammed110051@gmail.com>
2023-08-11 22:26:12 +0400

    Note Welcoming Part

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.Diagnostics;
+namespace cSharp_SimpleNoteApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      Welcome To    ");
+            Console.WriteLine(" +-+-+-+-+-+-+-+-+-+\r\n |W|h|a|t|I|s|F|u|n|\r\n +-+-+-+-+-+-+-+-+-+");
+            Console.WriteLine("        Notes      \n");
+
+        }
+    }
+}
